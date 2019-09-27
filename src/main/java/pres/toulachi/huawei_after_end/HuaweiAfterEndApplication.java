2849a82beb6e2e13979e4508bb3ca4faa179205a
Toulachi <821064692@qq.com>
2019-09-28 03:44:00 +0800

    Initial commit

---
+++ src/main/java/pres/toulachi/huawei_after_end/HuaweiAfterEndApplication.java
@@ -0,0 +1,13 @@
+package pres.toulachi.huawei_after_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuaweiAfterEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuaweiAfterEndApplication.class, args);
+    }
+
+}
